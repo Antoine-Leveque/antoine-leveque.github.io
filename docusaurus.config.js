2bef5177660e3b640da4ef25ec9f9fe441440455
Antoine LEVEQUE <antoine.leveque@viacesi.fr>
2022-11-10 14:52:37 +0100

    Add tool

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Awesome docusaurus site',
+  title: 'Antoine LEVEQUE',
   tagline: 'Dinosaurs are cool',
   url: 'https://antoine-leveque.github.io',
   baseUrl: '/',
@@ -59,7 +59,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Antoine LEVEQUE',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
