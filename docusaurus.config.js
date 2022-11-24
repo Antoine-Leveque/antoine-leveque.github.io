afb83a99022d1726a7d6ffba2e97329a51a4af39
Antoine LEVEQUE <antoine.leveque@viacesi.fr>
2022-11-24 15:03:28 +0100

    Update img

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
         title: '',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {to: '/blog', label: 'Blog', position: 'left'},
